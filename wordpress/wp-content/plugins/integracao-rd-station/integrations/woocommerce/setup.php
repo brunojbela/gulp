1ab88059366945bfa1f6ddf8081642ad2f31577c
brunojbela <bruno.jbela@gmail.com>
2018-03-10 14:48:17 -0300

    Gulp 1.0.1

--- wordpress/wp-content/plugins/integracao-rd-station/integrations/woocommerce/setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once('integration.php');
-
-$woocommerce_integration = new RDWoocommerceIntegration();
-$woocommerce_integration->add_callback('woocommerce_checkout_order_processed', 'send_lead_conversion');
